ba552f8575b996906f7909a8c2b9b8c1a7619c07
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2022-12-16 13:37:03 +0100

    vet inte hur jag ska titta efter levande celler

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -5,17 +5,58 @@ public class Model {
 
     int width;
     int height;
+    int size;
 
     public Model(int width, int height) {
         this.width = width;
         this.height = height;
+        this.size = width * height;
     }
 
     public void update() {
+
     }
 
     public Shape[] getShapes() {
-        Point[] points = {new Point(5,5), new Point (6, 4), new Point (6, 5), new Point (5, 4)};
+        isAlive(width, height, shapes)
+        Point[] points = {new Point(5,5), new Point (6, 5), new Point (7, 5)};
         return (Shape[])points;
+    }
+
+
+
+    protected int isAlive(int x, int y, byte[] d){
+
+        int count = 0;
+        int pos1 = y * width + x;
+
+        for ( int i = x-1; i <= x + 1; i++ ){
+
+            for ( int j = y - 1; j <= y + 1; j++ ){
+                int pos = j * width + i;
+
+                if ( pos >= 0 && pos < size - 1 && pos != pos1){
+
+                    if ( d[pos] == 1 ){
+
+                        count++;
+                    }
+                }
+            }
+        }
+        //dead
+
+        if ( d[pos1] == 0 ){
+            if ( count == 3 ){//becomes alive.
+                return 1;
+            }
+            return 0;//still dead
+
+        }else{//live
+            if ( count < 2 || count > 3 ){//Dies
+                return 0;
+            }
+            return 1;//lives
+        }
     }
 }
