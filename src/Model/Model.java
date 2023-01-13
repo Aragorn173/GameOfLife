1f3e5b8a69a826ed5c434ce3be20ad3b6368148f
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2023-01-13 13:37:44 +0100

    har en cell som blir levande och sedan en for-loop som går igenom varje ruta och ser om cellen lever eller är död. nästa gång: göra att när den hittar en levande cell så blir det en shape. (arraylist)

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,39 +1,40 @@
 package Model;
 import View.*;
 
-import java.util.ArrayList;
-
 public class Model {
 
     int width;
     int height;
-    int size;
-    int[] world = new int[size];
+
+    int[] cells = new int[50 * 50];
 
     public Model(int width, int height) {
         this.width = width;
         this.height = height;
-        this.size = width * height;
-        this.world = world;
+        cells[255] = 1;
     }
+
 
     public void update() {
-
+        int n = 0;
+        for (int i = 0; i < cells.length; i++) {
+            if (cells[i] == 1) {
+                n++;
+                System.out.println("isAlive");
+            } else {
+                System.out.println("isDead");
+            }
+        }
+        System.out.println(n);
     }
+
 
 
     public Shape[] getShapes() {
-        ArrayList <Point> points = new ArrayList<Point>();
-        points.add(new Point (6, 5));
-        points.add(new Point (5, 5));
+
+        Point[] points = {new Point(5,5)};
         return (Shape[])points;
     }
 
-    public void checkCell(){
-        for(int i = 0; i < size; i++) {
-
-        }
-
-    }
 
 }
