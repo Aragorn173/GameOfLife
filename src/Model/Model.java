ee721182bdf20af2cd7791da95b56ceee7d8e0a4
Aragorn173 <77720219+Aragorn173@users.noreply.github.com>
2023-01-19 13:46:03 +0100

    jag tror att den kan hitta alla levande celler och sedan stoppar in positionen av dem i en ny array som är "alive". nästa måste jag fixa getshapes och se hur man kollar efter alla grannar.

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,12 +1,18 @@
 package Model;
 import View.*;
 
+import java.util.ArrayList;
+
 public class Model {
 
     int width;
     int height;
 
+    int numalive = 0;
+
+
     int[] cells = new int[50 * 50];
+    int[] alive = new int [numalive];
 
     public Model(int width, int height) {
         this.width = width;
@@ -16,25 +22,52 @@ public class Model {
 
 
     public void update() {
-        int n = 0;
-        for (int i = 0; i < cells.length; i++) {
-            if (cells[i] == 1) {
-                n++;
-                System.out.println("isAlive");
-            } else {
-                System.out.println("isDead");
-            }
-        }
-        System.out.println(n);
+        checkCells();
     }
 
 
 
     public Shape[] getShapes() {
 
-        Point[] points = {new Point(5,5)};
-        return (Shape[])points;
+        ArrayList<Point> points = new ArrayList<Point>();
+
+        points.add(new Point(7, 8));
+
+
+        Point[] pixelsToDisplay = new Point[points.size()];
+        points.toArray(pixelsToDisplay);
+
+        return (Shape[]) points;
     }
 
+    
+    public void checkCells() {
+        int n = 0;
+        int x;
+
+        for (int z = 0; z < cells.length; z++) {
+            if (cells[z] == 1) {
+                numalive++;
+            }
+        }
+
+
+        for (int i = 0; i < cells.length; i++) {
+            if (cells[i] == 1) {
+                x = i;
+                alive[n] = x;
+                n++;
+            } else {
+                System.out.println("isDead");
+            }
+        }
+
+        System.out.println(n);
+    }
+
+    
+    public void checkNeighbour() {
+
+    }
 
 }
